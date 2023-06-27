1dc5bb4361db994627a9abfe12478e861d79488c
matthcol <matthias@learning64.net>
2023-06-27 11:22:41 +0200

    Makefile ok

--- complex_type/array.h
+++ complex_type/array.h
@@ -6,8 +6,28 @@
 #define N 10
 
 // array with fixed size
+
+/**
+ * @brief 
+ * 
+ * @param tab 
+ * @param value 
+ */
 void fill_n(double tab[N], double value);
+
+/**
+ * @brief 
+ * 
+ * @param tab 
+ * @return double 
+ */
 double sum_n(double tab[N]);
+
+/**
+ * @brief 
+ * 
+ * @param tab 
+ */
 void display_n(double tab[N]);
 
 void fill(double tab[], size_t n, double value);
@@ -19,7 +39,22 @@ void display(double tab[], size_t n);
 // void display(double *tab, size_t n);
 
 // 2D
+
+/**
+ * @brief 
+ * 
+ * @param tab 
+ * @param rows 
+ */
 void display2D(double tab[][N], size_t rows);
+
+/**
+ * @brief 
+ * 
+ * @param tab 
+ * @param rows 
+ * @param cols 
+ */
 void display2Ddyn(double *tab, size_t rows, size_t cols);
 
 #endif // !_ARRAY_H
